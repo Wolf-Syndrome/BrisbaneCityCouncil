d01005915a2011e531b5fb9f8bd8bb4c7cc0aa22
wolf-syndrome <wolfhunter7521@gmail.com>
2024-06-02 17:21:45 +1000

    updated work to do

--- todo.md
+++ todo.md
@@ -10,4 +10,23 @@
 2. PHP
 3. Database
 4. Some example filters -> location, title, event type, id
-5. 
+5. Finish the get_event_by_id
+6. Have a simple page for a user subscribing to an event, (two input boxes and button) (goes to sql server)
+7. Write some logic to "send an email" -> setup template email, (call db for all userevents, for each row, search through the xml for the id, than just echo the to and message)
+To: 04177223
+Message:
+    ksmakljdlksajd
+    sdadka
+
+    lskadlkad
+
+<br><br>
+
+To: test@gmail.com
+Message:
+    ksmakljdlksajd
+    sdadka
+
+    lskadlkad
+
+<br><br>
--- website/get_event_by_info.php
+++ website/get_event_by_info.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $suburbs = isset($_POST["suburbs"]) ? $_POST["suburbs"] : [];
     $event_types = isset($_POST["event_types"]) ? $_POST["event_types"] : [];
 
-    
+
     if ($useAPI) {
         $url = 'http://www.trumba.com/calendars/brisbane-city-council.xml';
         $curl = curl_init();
@@ -36,6 +36,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $searchFor = "Brisbane";
     $entries = $xml->entry;
     for ($i = 0; $i < count($entries); $i++) {
+        // Checking title
+        $entry = $entries[$i];
+        if ($title !== "") {
+            if (!str_contains($entry->title, $title)) {
+                continue;
+            }
+        }
+
+        // Checking suburb
+        if (count($suburbs) > 0) {
+            $hasSuburb = false;
+            foreach ($suburbs as $suburb) {
+                if (str_contains($entry->xpath('//gc:venueaddress')[0], $suburb)) {
+                    $hasSuburb = true;
+                }
+            }
+            if (!$hasSuburb) {
+                continue;
+            }
+        }
+        
+
+        print_r($entry);
+
         if (str_contains($entries[$i]->title, $searchFor)) {
             print_r($entries[$i]);
         }
